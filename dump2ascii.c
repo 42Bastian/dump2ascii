630a236a572d6703db99f25fb2cce13cc775ae83
42Bastian <42Bastian@users.noreply.github.com>
2016-11-03 18:32:23 +0100

    clean-up and big-endian
    remove unused variables
    added readNum to support big/little endian support (global setting)
    differ lower/upper case modifiers for x,a and p for lower/upper case hex numbers
    use stdint

--- dump2ascii.c
+++ dump2ascii.c
@@ -11,59 +11,105 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <stdint.h>
 
 #define MAX_VARS 32
 #define PUTCHAR(c) fputc(c,out)
 
-static const char hex[16] = {
-  '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
-};
+/**
+ * Read 64,32 or 16 bit number from stream with correct endianess.
+ *
+ */
+uint64_t readNum(FILE *in, int size, int endian, int *err)
+{
+  uint64_t n64;
+  int c;
+  int i;
+
+  *err = 0;
+  n64 = 0;
+
+  if ( endian == BIG_ENDIAN ){
+    for(i = 0; i < size; ++i){
+      n64 <<= 8;
+      c = getc(in);
+      if ( c == EOF ){
+        *err = 1;
+        n64 = 0;
+        break;
+      }
+      n64 |= c & 0xff;
+    }
+  } else {
+    for(i = 0; i < size; ++i){
+      n64 >>= 8;
+      c = getc(in);
+      if ( c == EOF ){
+        *err = 1;
+        n64 = 0;
+        break;
+      }
+      n64 |= ((uint64_t)(c & 0xff))<<(8*(size-1));
+    }
+  }
+  return n64;
+}
 
 void dump(FILE *in,
           FILE *out,
           const char *fmt,
           int len,
-          int skip)
+          int skip,
+          int endian)
 {
+  static const char hex[16] = {
+    '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'
+  };
+  static const char HEX[16] = {
+    '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+  };
+
   const char *pf;
+
   int c,f;
   char fill;
   char *p;
-  const char *fp;
   int width;
   char h[64];
-  unsigned long n = 0;
-  unsigned long long n64;
+  uint64_t n64;
   int sign;
+
   ssize_t err;
   int line;
   unsigned long pos;
+
   const char *loopStart;
   int loopCount;
   int loopCountMax;
+
   unsigned long long vars[MAX_VARS];
-  int varSize[MAX_VARS];
   int varCount;
   int varRef;
 
   pos = 0;
+  line = 1;
+  varRef = -1;
+  varCount = 0;
+  loopCount = -1;
+  loopCountMax = 0;
+  pf = fmt;
+  c = 0;
+
   if ( skip > len ){
     skip = len;
   }
-  c = 0;
+
   while( skip && c != EOF ){
     c = getc(in);
     ++pos;
     --len;
     --skip;
   }
-
-  c = 0;
-  line = 1;
-  varRef = -1;
-  varCount = 0;
-  loopCount = -1;
-  pf = fmt;
 
   while ( len || *pf ){
     do{
@@ -138,8 +184,8 @@ void dump(FILE *in,
     }
 
     /* get data from stream or variable */
+    n64 = 0;
     if ( c == 'x' || c == 'X' || c == 'p' || c == 'P' || c == 'd' || c == 'u' ){
-      n64 = 0;
       if ( varRef != -1 ){
         if ( varRef < varCount ){
           n64 = vars[varRef];
@@ -149,11 +195,10 @@ void dump(FILE *in,
         switch ( f ){
         case 0:
         case 3:
-          err = fread((void *)&n64, sizeof(n), 1, in);
-          pos += sizeof(n);
-          len -= sizeof(n);
-          if ( err != 1 ){
-            n64 = 0;
+          n64 = readNum(in, 4, endian, &err);
+          pos += 4;
+          len -= 4;
+          if ( err ){
             len = 0;
           }
           break;
@@ -167,20 +212,18 @@ void dump(FILE *in,
           }
           break;
         case 2:
-          err = fread((void *)&n64, 2, 1, in);
+          n64 = readNum(in, 2, endian, &err);
           pos += 2;
           len -= 2;
-          if ( err != 1 ){
-            n64 = 0;
+          if ( err ){
             len = 0;
           }
           break;
         case 4:
-          err = fread((void *)&n64, sizeof(n64), 1, in);
-          pos += sizeof(n64);
-          len -= sizeof(n64);
-          if ( err != 1 ){
-            n64 = 0;
+          n64 = readNum(in, 8, endian, &err);
+          pos += 8;
+          len -= 8;
+          if ( err  ){
             len = 0;
           }
           break;
@@ -188,7 +231,6 @@ void dump(FILE *in,
 
         if ( varCount < MAX_VARS ){
           vars[varCount] = n64;
-          varSize[varCount] = f;
           ++varCount;
         }
       } else {
@@ -215,7 +257,7 @@ void dump(FILE *in,
       continue;
 
     case '}':
-      if ( loopCount < loopCountMax ){
+      if ( loopCountMax ){
         ++loopCount;
         if ( loopCount < loopCountMax ){
           pf = loopStart;
@@ -233,6 +275,9 @@ void dump(FILE *in,
         --width;
         ++pos;
       }
+      if ( err == EOF ){
+        len = 0;
+      }
       continue;
 
     case 'a':
@@ -240,6 +285,15 @@ void dump(FILE *in,
         sprintf(h,"%%%c%dx",fill,width);
       } else {
         strcpy(h,"%08x");
+      }
+      fprintf(out,h,pos);
+      continue;
+
+    case 'A':
+      if ( width ){
+        sprintf(h,"%%%c%dX",fill,width);
+      } else {
+        strcpy(h,"%08X");
       }
       fprintf(out,h,pos);
       continue;
@@ -254,14 +308,21 @@ void dump(FILE *in,
       continue;
 
     case 'x':
-    case 'X':
     case 'p':
-    case 'P':
       do {
         *p++ = hex[(n64 & 0xfULL)];
         n64 >>= 4;
       } while (n64);
       break;
+
+    case 'X':
+    case 'P':
+      do {
+        *p++ = HEX[(n64 & 0xfULL)];
+        n64 >>= 4;
+      } while (n64);
+      break;
+
     case 'd':
     case 'u':
       sign = 0;
@@ -302,6 +363,7 @@ void dump(FILE *in,
         *p++ = '-';
       }
       break;
+
     case 'c':
       c = 0;
       if ( varRef != -1 ){
@@ -323,6 +385,7 @@ void dump(FILE *in,
       }
       *p++ = c;
       break;
+
     case 's':
       p = h;
       do {
@@ -334,20 +397,17 @@ void dump(FILE *in,
           len = 0;
         }
         *p++ = c;
-      }while( c );
-
-      p = h;
+      }while( c && p < h+sizeof(h));
+      *p = 0;
 
       width = ( width > (sizeof(h)-1) ) ? (sizeof(h)-1) : width;
 
-      for( width -= strlen(p); width > 0 ; --width ){
+      for( width -= strlen(h); width > 0 ; --width ){
         PUTCHAR(fill);
       }
-      while( *p ){
-        PUTCHAR(*p);
-        ++p;
-      }
+      fprintf(out,"%s",h);
       continue;
+
     default:
       PUTCHAR(c);
       continue;
@@ -375,29 +435,33 @@ main (int argc, char *argv[])
   FILE *out = stdout;
   char *format = NULL;
   int skip = 0;
-  while ((c = getopt (argc, argv, "hi:o:f:s:")) != -1) {
+  int endian = LITTLE_ENDIAN;
+
+  while ((c = getopt (argc, argv, "bhi:o:f:s:")) != -1) {
     switch (c) {
     case 'h':
       fprintf(stderr,
-              "Usage: %s [-i <input>] [-o <output>] [-s <skip>] [-f <format>]\n",argv[0]);
+              "Usage: %s [-i <input>] [-o <output>] [-b] [-s <skip>] [-f <format>]\n",argv[0]);
       fprintf(stderr,
               "input:  stdin if not set\n"
               "output: stdout if not set\n"
               "format: normal printf() format (no float), default %%ld\n"
-              "        extra: %%[fill][size]q outputs line number\n"
-              "               %%[fill][size]a outputs input stream  position\n"
+              "        extra: %%[fill][size]q outputs line number decimal\n"
+              "               %%[fill][size](a|A) outputs input stream position in hex\n"
               "               %%[size]j skips bytes in the input stream\n"
-              "               Typemodifiers: b - byte: %02bx\n"
-              "                              h - short: %04hx\n"
+              "               Typemodifiers: b - byte: %%02bx\n"
+              "                              h - short: %%04hx\n"
               "               %%[size]$ back-reference to a previous %%d|%%x\n"
               "               %%0$ use loop counter as index\n"
               "skip: skip bytes at beginning of file\n"
+              "-b: Select big endian reading\n"
               "Example\n"
               "-f \"%%08a: %%16{ %%02bx%%} : %%16{%%0$%%c%%}\\n\" for canonical dump:\n"
               "00000000:  23 69 6E 63 6C 75 64 65 20 3C 73 74 64 69 6F 2E : #include <stdio.\n"
               "00000010:  68 3E 0D 0A 23 69 6E 63 6C 75 64 65 20 3C 73 74 : h>..#include <st\n"
         );
       return 0;
+
     case 'i':
       if (!(in = fopen (optarg, "rb"))) {
         perror ("Input file open");
@@ -406,16 +470,25 @@ main (int argc, char *argv[])
       len = ftell(in);
       fseek(in,0L,SEEK_SET);
       break;
+
     case 'o':
       if (!(out = fopen (optarg, "wb"))) {
         perror ("Input file open");
       }
       break;
+
     case 'f':
       format = optarg;
       break;
+
     case 's':
       skip = atoi(optarg);
+      break;
+
+    case 'b':
+      endian = BIG_ENDIAN;
+      break;
+
     default:
       break;
     }
@@ -427,6 +500,7 @@ main (int argc, char *argv[])
       format = "%ld";
     }
   }
-  dump(in, out, format, len, skip);
+
+  dump(in, out, format, len, skip, endian);
   return 0;
 }
